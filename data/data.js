c18bc24e1fab0ae69faac7fa75847506be76011c
Livingstone Maghanga <stoneriga@gmail.com>
2023-06-16 19:42:10 +0300

    Recent Order and Bar Chart components

---
+++ data/data.js
@@ -0,0 +1,113 @@
+export const data = [
+    {
+        id: 1,
+        name: {
+            first: 'Livingstone',
+            last: 'Maghanga'
+        },
+        total: 2795.78,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '15 Minutes ago'
+    },
+    {
+        id: 2,
+        name: {
+            first: 'Wilstone ',
+            last: 'Kichunju'
+        },
+        total: 24322.00,
+        status: 'Processing',
+        method: 'PayPal',
+        date: '23 Minutes ago'
+    },
+    {
+        id: 3,
+        name: {
+            first: 'Christine',
+            last: 'Wambugha'
+        },
+        total: 3565.50,
+        status: 'Completed',
+        method: 'PayPal',
+        date: '30 Minutes ago'
+    },
+    {
+        id: 4,
+        name: {
+            first: 'Priscillah',
+            last: 'Samba'
+        },
+        total: 10500.00,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '50 Minutes ago'
+    },
+    {
+        id: 5,
+        name: {
+            first: 'Joseph',
+            last: 'Mzame'
+        },
+        total: 25000.95,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '5 Minutes ago'
+    },
+    {
+        id: 6,
+        name: {
+            first: 'Evans',
+            last: 'Mwarabu'
+        },
+        total: 30000.00,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '5 Hours ago'
+    },
+    {
+        id: 7,
+        name: {
+            first: 'Tim',
+            last: 'Thomas'
+        },
+        total: 659.45,
+        status: 'On Hold',
+        method: 'MasterCard',
+        date: '3 Hours ago'
+    },
+    {
+        id: 8,
+        name: {
+            first: 'Haley',
+            last: 'Whiting'
+        },
+        total: 5550.64,
+        status: 'On Hold',
+        method: 'Visa',
+        date: '45 Minutes ago'
+    },
+    {
+        id: 9,
+        name: {
+            first: 'El-Hajj Malik',
+            last: 'El-Shabaaz'
+        },
+        total: 1000000.00,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '1 Hour ago'
+    },
+    {
+        id: 10,
+        name: {
+            first: 'Alice',
+            last: 'Mwangombe'
+        },
+        total: 4678.45,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '15 Minutes ago'
+    },
+
+]
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Image from 'next/image'
 
 import Header from '@/components/Header'
 import TopCards from '@/components/TopCards'
+import BarChart from '@/components/BarChart'
+import RecentOrders from '@/components/RecentOrders'
+
 
 
 export default function Home() {
@@ -9,6 +12,10 @@ export default function Home() {
     <main className= "bg-gray-100 min-h-screen">
       <Header />
       <TopCards />
+      <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
+        <BarChart />
+        <RecentOrders/>
+      </div>
     </main>
   )
 }
